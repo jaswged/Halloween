9047317863d9ef135db7c3eb22aa87d4da196ff5
Jason <jaswged@hotmail.com>
2015-10-29 13:36:36 -0500

    setup the scenes for a build

--- Assets/Scripts/PumpkinTrigger.cs
+++ Assets/Scripts/PumpkinTrigger.cs
@@ -5,7 +5,6 @@ public class PumpkinTrigger : MonoBehaviour {
     void OnTriggerEnter(Collider other) {
         if(other.CompareTag("Player")) {
             GameManager.manager.PumpkinFound(this);
-#warning should pool the pumpkins if we do the pickup as many as you can idea.
             //Destroy(gameObject);
         }
     }
